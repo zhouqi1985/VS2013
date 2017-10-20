c49fcd98564a85d70df29a21345d2e9c7b0a39c9
zhouqi1985 <568385076@qq.com>
2017-10-20 12:21:23 +0800

    new WeaponBuildEvents

---
+++ WeaponBuildEvents/WeaponBuildEvents/WeaponBuildEvents.DAL.AdminWCF.Client/IWeaponBuildEventsAdminServiceClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeaponBuildEvents.DAL.AdminWCF.Client
+{
+    public interface IWeaponBuildEventsAdminServiceClient
+    {
+    }
+}
---
+++ WeaponBuildEvents/WeaponBuildEvents/WeaponBuildEvents.DAL.WCF.Client/IWeaponBuildEventsServiceClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeaponBuildEvents.DAL.WCF.Client
+{
+    public interface IWeaponBuildEventsServiceClient
+    {
+    }
+}
